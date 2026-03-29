914238f06c789e5d4dc788aec562bbd602ccc909
Koehler <bernardoskoehler@gmail.com>
2026-03-29 19:36:30 -0300

    Implement main method for Biblioteca operations

--- v1-library-system/scr/Main.java
+++ v1-library-system/scr/Main.java
@@ -1,1 +1,14 @@
+public static void main(String[] args) {
+    Biblioteca b = new Biblioteca();
+    Padrao[] p = {
+      new Padrao("Bruno"),
+      new Padrao("Bernardo"),
+      new Padrao("Beico")
+    };
 
+    p[0].alugar(0);
+    p[0].alugar(0);
+    p[0].devolver(0);
+    b.mostrarLista();
+    b.mostarLivro(0);
+}
